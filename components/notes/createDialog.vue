129b5337efb1b24f566fabdba0dbc004f1d24bbc
kom50 <omprakashkartik82280@gmail.com>
2023-04-30 14:15:36 +0530

    Added notes create validation

--- components/notes/createDialog.vue
+++ components/notes/createDialog.vue
@@ -7,33 +7,36 @@
             </v-btn>
         </template>
         <v-card class="pa-2">
-            <v-card-title>
-                <span class="text-h5">Create Note</span>
-            </v-card-title>
-            <v-container>
-                <v-row>
-                    <v-col cols="12">
-                        <v-text-field v-model="title" variant="outlined" label="Title*" required clearable></v-text-field>
-                    </v-col>
-                    <v-col cols="12">
-                        <v-text-field v-model="subtitle" variant="outlined" label="Subtitle" clearable></v-text-field>
-                    </v-col>
-                    <v-col cols="12">
-                        <v-textarea v-model="description" variant="outlined" label="Description*" persistent-hint clearable
-                            required></v-textarea>
-                    </v-col>
-                </v-row>
-            </v-container>
-            <v-card-text>
-                <small>*indicates required field</small>
-            </v-card-text>
-            <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="red" variant="flat" @click="dialog = false">
-                    Close
-                </v-btn>
-                <v-btn color="blue" variant="tonal" @click="save"> Save </v-btn>
-            </v-card-actions>
+            <v-form @submit.prevent="save">
+                <v-card-title>
+                    <span class="text-h5">Create Note</span>
+                </v-card-title>
+                <v-container>
+                    <v-row>
+                        <v-col cols="12">
+                            <v-text-field v-model="title" variant="outlined" label="Title*" required :rules="rules.title"
+                                clearable></v-text-field>
+                        </v-col>
+                        <v-col cols="12">
+                            <v-text-field v-model="subtitle" variant="outlined" label="Subtitle" clearable></v-text-field>
+                        </v-col>
+                        <v-col cols="12">
+                            <v-textarea v-model="description" variant="outlined" label="Description*"
+                                :rules="rules.description" clearable required></v-textarea>
+                        </v-col>
+                    </v-row>
+                </v-container>
+                <v-card-text>
+                    <small>*indicates required field</small>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="red" variant="flat" @click="dialog = false">
+                        Close
+                    </v-btn>
+                    <v-btn type="submit" color="blue" variant="tonal"> Save </v-btn>
+                </v-card-actions>
+            </v-form>
         </v-card>
     </v-dialog>
 </template>
@@ -54,10 +57,30 @@ const title = ref('')
 const subtitle = ref('')
 const description = ref('')
 
+/** Input fields rules */
+const rules = {
+    title: [
+        (value: string) => {
+            if (value) return true
+            return 'You must enter a note title.'
+        }
+    ],
+    description: [
+        (value: string) => {
+            if (value) return true
+            return 'You must enter a note description.'
+        }
+    ],
+}
+
 /**
  * Function to save note data
  */
-async function save() {
+async function save(event: any) {
+    const results = await event
+
+    // If input not valid
+    if (!results.valid) return
 
     const note: Note = {
         title: title.value,
@@ -70,5 +93,13 @@ async function save() {
     // Store in IDB and also in note store
     await storeNote(note)
     noteStore.setNote(note)
+
+    // CLose create dialog box
+    dialog.value = false
+
+    // Reset value
+    title.value = ''
+    subtitle.value = ''
+    description.value = ''
 }
 </script>
